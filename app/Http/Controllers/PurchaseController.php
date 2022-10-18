c46f203db36e4a4bd5d01a39ec0d9b7dfea20b80
yasu2122yasu <ya2su1shi2@icloud.com>
2022-10-18 11:53:22 +0900

    micromodalの実装

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -8,6 +8,7 @@ use App\Models\Purchase;
 use App\Models\Customer;
 use App\Models\Item;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseController extends Controller
 {
@@ -46,7 +47,27 @@ class PurchaseController extends Controller
      */
     public function store(StorePurchaseRequest $request)
     {
-        //
+        DB::beginTransaction();
+
+        try {
+            $purchase = Purchase::create([
+                'customer_id' => $request->customer_id,
+                'status' => $request->status
+            ]);
+
+            foreach ($request->items as $item) {
+                $purchase->items()->attach($purchase->id, [
+                    'item_id' => $item['id'],
+                    'quantity' => $item['quantity']
+                ]);
+            }
+
+            DB::commit();
+
+            return to_route('dashboard');
+        } catch (\Exception $e) {
+            DB::rollback();
+        }
     }
 
     /**
