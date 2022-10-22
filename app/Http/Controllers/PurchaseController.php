69051188f1397a7f2b8bc07a0d223743d5633c7c
yasu2122yasu <ya2su1shi2@icloud.com>
2022-10-22 23:32:40 +0900

    購入履歴画面の更新

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -92,7 +92,7 @@ class PurchaseController extends Controller
         $order = Order::groupBy('id')
             ->where('id', $purchase->id)
             ->selectRaw('id, sum(subtotal) as total,
-        customer_name, status, created_at')
+            customer_name, status, created_at')
             ->get();
 
         return Inertia::render('Purchases/Show', [
@@ -109,7 +109,38 @@ class PurchaseController extends Controller
      */
     public function edit(Purchase $purchase)
     {
-        //
+        $purchase = Purchase::find($purchase->id);
+
+        $allItems = Item::select('id', 'name', 'price')
+            ->get();
+
+        $items = [];
+
+        foreach ($allItems as $allItem) {
+            $quantity = 0;
+            foreach ($purchase->items as $item) {
+                if ($allItem->id === $item->id) {
+                    $quantity = $item->pivot->quantity;
+                }
+            }
+            array_push($items, [
+                'id' => $allItem->id,
+                'name' => $allItem->name,
+                'price' => $allItem->price,
+                'quantity' => $quantity,
+            ]);
+        }
+
+        $order = Order::groupBy('id')
+            ->where('id', $purchase->id)
+            ->selectRaw('id, customer_id,
+            customer_name, status, created_at')
+            ->get();
+
+        return Inertia::render('Purchases/Edit', [
+            'items' => $items,
+            'order' => $order
+        ]);
     }
 
     /**
